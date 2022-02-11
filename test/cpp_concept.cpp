bdcd063e597d101f8a9830d62fde8a6aeac4c1fd
Sebastian Bischlager <sebastian-bischlager@web.de>
2022-02-11 16:24:36 +0100

    Added test case for abbreviated function templates

--- test/cpp_concept.cpp
+++ test/cpp_concept.cpp
@@ -60,3 +60,28 @@ void a(T t);
     cpp_entity_index idx;
     parse(idx, "cpp_function_template_concept.cpp", code, false, cpp_standard::cpp_20);
 }
+
+TEST_CASE("cpp_function_template_concept_abbreviated")
+{
+    auto code = R"(
+#include <concepts>
+
+class Base {};
+class Derived : public Base {};
+
+template <typename T>
+concept BaseType = std::derived_from<T, Base>;
+
+void a(auto t);
+void b(BaseType auto t);
+void c(BaseType auto... t);
+void d(const BaseType auto* t);
+void e(BaseType auto& t);
+
+template <>
+void d<Derived>(const Derived* t);
+)";
+
+    cpp_entity_index idx;
+    parse(idx, "cpp_function_template_concept_abbreviated.cpp", code, false, cpp_standard::cpp_20);
+}
